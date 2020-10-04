22ffc9744020384968d43433f05afa5ca04728b5
Diego Cantero <diego.cantero@talent.com>
2020-10-03 21:29:08 -0500

    Null correct

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('user_type')->constrained('types')->nullable(); 
+            $table->foreignId('user_type')->nullable()->constrained('types'); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
