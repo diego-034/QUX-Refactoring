42a679ce9c814831ffdd421f7007a4634c673e52
Diego Cantero <diego.cantero@talent.com>
2020-10-04 08:38:47 -0500

    Models and Show

--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'image', 'description','state','color','price','iva','discount','size_s','size_m','size_l','user_id'
+    ];
 }
