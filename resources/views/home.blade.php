8de993859dda18e1d272528f1c7a4d03501d97dc
Jhon <ztheeban205@gmail.com>
2020-10-03 20:57:16 -0500

    Botones Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,31 @@
-//Poner de la pagina base
+<!--Poner de la pagina base -->
+<link href="{{ asset('css/home.css') }}" rel="stylesheet">
 @extends('layouts.app')
-    //Parte que se incluye  en la pagina base
+    <!--Parte que se incluye  en la pagina base -->
     @section('content')
-        @guest
-        //Parte que ve el ususario no registrado
-            <h1>Home</h1>
-        @else
-        //Parte que ve el usuario registrado
-            <h3>Home {{ Auth::user()->name }}</h3>
+        <div class="jumbotron text-center">
+            <div class="container">
+                    <h1 class="display-4 text-white"><strong>Q-UX</strong></h1>
+                    <p class="text-white"><strong>Diseña Tu Camisa</strong></p>
+                @guest
+                    <!-- Parte que ve el ususario no registrado -->
+                    
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <a href="/login"><button type="button" class="btn btn-outline-light">Iniciar Sesión</button></a>
+                    </div>
+                @else
+                    <!-- 
+                        Parte que ve el usuario registrado 
+                        Botones que nos dirigiran a las distintas vistas    
+                    -->
+                    <h3 class="text-white">Bienvenido {{ Auth::user()->name }}</h3>
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <a href="/" class="mr-2"><button type="button" class="btn btn-outline-light">Productos</button></a>
+                        <a href="/" class="mr-2"><button type="button" class="btn btn-outline-light">Pedidos</button></a>
+                        <a href="/"><button type="button" class="btn btn-outline-light">Perfil</button></a>
+                    </div>
 
-        @endguest
+                @endguest
+            </div>
+        </div>
     @endsection
