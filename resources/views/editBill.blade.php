2755e44959e15bfacf33d4d76fe71dc17c7af674
imJac0b <jacobojimenez08@gmail.com>
2020-10-05 19:04:55 -0500

    editar factura

--- resources/views/editBill.blade.php
+++ resources/views/editBill.blade.php
@@ -5,7 +5,7 @@
     <div class="form-v10-content">
         <form class="form-detail" action="#" method="post" id="myform" style="margin: 0;">
             <div class="form-left">
-                <h2>Product Infomation</h2>                
+                <h2>Información del producto</h2>                
                 <div class="form-group">
                     <div class="form-row form-row-1">
                         <input type="text" name="first_name" id="first_name" class="input-text" placeholder="Cod. producto" required>
@@ -15,93 +15,35 @@
                     </div>
                 </div>
                 <div class="form-row">
-                    <select name="position">
-                        <option value="position">Position</option>
-                        <option value="director">Director</option>
-                        <option value="manager">Manager</option>
-                        <option value="employee">Employee</option>
-                    </select>
-                    <span class="select-btn">
-                        <i class="zmdi zmdi-chevron-down"></i>
-                    </span>
-                </div>
-                <div class="form-row">
                     <input type="number" name="company" class="company" id="company" placeholder="Cantidad" required>
-                </div>
-                <div class="form-group">
-                    <div class="form-row form-row-3">
-                        <input type="text" name="business" class="business" id="business" placeholder="Business Arena" required>
-                    </div>
-                    <div class="form-row form-row-4">
-                        <select name="employees">
-                            <option value="employees">Employees</option>
-                            <option value="trainee">Trainee</option>
-                            <option value="colleague">Colleague</option>
-                            <option value="associate">Associate</option>
-                        </select>
-                        <span class="select-btn">
-                            <i class="zmdi zmdi-chevron-down"></i>
-                        </span>
-                    </div>
                 </div>
             </div>
 
 
             
             <div class="form-right">
-                <h2>Contact Details</h2>
+                <h2>Detalles de la factura</h2>
                 <div class="form-row">
-                    <input type="text" name="street" class="street" id="street" placeholder="Street + Nr" required>
+                    <input type="text" name="street" class="street" id="street" placeholder="Total" value="{{$response->total}}" required>
                 </div>
                 <div class="form-row">
-                    <input type="text" name="additional" class="additional" id="additional" placeholder="Additional Information" required>
+                    <input type="text" name="additional" class="additional" id="additional" placeholder="Descuento" value="{{$response->discount}}" required>
                 </div>
                 <div class="form-group">
                     <div class="form-row form-row-1">
-                        <input type="text" name="zip" class="zip" id="zip" placeholder="Zip Code" required>
+                        <input type="text" name="iva" class="zip" id="iva" placeholder="iva" value="{{$response->iva}}" required>
                     </div>
-                    <div class="form-row form-row-2">
-                        <select name="place">
-                            <option value="place">Place</option>
-                            <option value="Street">Street</option>
-                            <option value="District">District</option>
-                            <option value="City">City</option>
-                        </select>
-                        <span class="select-btn">
-                            <i class="zmdi zmdi-chevron-down"></i>
-                        </span>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <select name="country">
-                        <option value="country">Country</option>
-                        <option value="Vietnam">Vietnam</option>
-                        <option value="Malaysia">Malaysia</option>
-                        <option value="India">India</option>
-                    </select>
-                    <span class="select-btn">
-                        <i class="zmdi zmdi-chevron-down"></i>
-                    </span>
                 </div>
                 <div class="form-group">
                     <div class="form-row form-row-1">
-                        <input type="text" name="code" class="code" id="code" placeholder="Code +" required>
+                        <input type="text" name="state" class="code" id="state" placeholder="Estado" value="{{$response->state}}" required>
                     </div>
                     <div class="form-row form-row-2">
-                        <input type="text" name="phone" class="phone" id="phone" placeholder="Phone Number" required>
+                        <input type="text" name="size" class="phone" id="size" placeholder="Talla" value="{{$response->size}}" required>
                     </div>
-                </div>
-                <div class="form-row">
-                    <input type="text" name="your_email" id="your_email" class="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Your Email">
-                </div>
-                <div class="form-checkbox">
-                    <label class="container"><p>I do accept the <a href="#" class="text">Terms and Conditions</a> of your site.</p>
-                        <input type="checkbox" name="checkbox">
-                        <span class="checkmark"></span>
-                    </label>
                 </div>
                 <div class="form-row-last">
-                    <input type="submit" name="register" class="register" value="Register Badge">
+                    <input type="submit" name="register" class="register" value="Guardar">
                 </div>
             </div>
         </form>
